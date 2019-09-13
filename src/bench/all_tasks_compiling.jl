0f1c498fcaf7a81e1f03e200aa23ad837d04950a
Nathan Daly <nhdaly@gmail.com>
2019-09-13 14:32:09 -0400

    Add "all tasks compiling" benchmark, disabled due to julia race bug

---
+++ src/bench/all_tasks_compiling.jl
@@ -0,0 +1,24 @@
+#
+
+include("../common.jl")
+
+function work(i, v, n)
+    out = v
+    for i in 1:n
+        mul = @eval (a) -> a*$v
+        out = Base.invokelatest(mul, out)
+    end
+    return out
+end
+
+#@time work(1,2,100)
+#
+#using PProf
+#PProf.clear(); @pprof @sync begin
+#    Threads.@spawn work(1, 2, 100)
+#    Threads.@spawn work(1, 2, 100)
+#    Threads.@spawn work(1, 2, 100)
+#    Threads.@spawn work(1, 2, 100)
+#end
+
+measure_work()
